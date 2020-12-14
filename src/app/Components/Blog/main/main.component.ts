6ef441d2f37602719e148e26107f5520216f5668
firatkaya1 <firat.kaya@mail.com>
2020-12-14 22:27:31 +0300

    added default pictures if not load

--- src/app/Components/Blog/main/main.component.ts
+++ src/app/Components/Blog/main/main.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  templateUrl: './main.component.html'
 })
 export class MainComponent implements OnInit {
 
